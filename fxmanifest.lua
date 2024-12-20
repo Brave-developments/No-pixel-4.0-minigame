11e159b667d3d5aea9b7a901f5c8f4646062f868
Brave Developments <89262662+Brave-developments@users.noreply.github.com>
2024-12-21 09:46:55 +1000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "cerulean"
+
+description "FiveM Minigames by Bulgar Development"
+version '1.0.0'
+
+lua54 'yes'
+
+games {
+  "gta5",
+}
+
+ui_page 'web/build/index.html'
+
+client_script "client/**/*"
+
+files {
+  'web/build/index.html',
+  'web/build/**/*',
+}
